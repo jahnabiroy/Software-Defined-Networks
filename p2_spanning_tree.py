d1ca23c905d48490a74cf476404a21ed3c266094
33Arsenic75 <122697564+33Arsenic75@users.noreply.github.com>
2024-09-27 19:53:36 +0530

    .

---
+++ p2_spanning_tree.py
@@ -0,0 +1,108 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.spanning_tree = self.load_spanning_tree('/tmp/spanning_tree.txt')
+
+    def load_spanning_tree(self, filepath):
+        """Load the spanning tree from a file."""
+        spanning_tree = []
+        with open(filepath, 'r') as f:
+            for line in f:
+                node1, node2 = line.strip().split()
+                spanning_tree.append((node1, node2))
+        return spanning_tree
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            # If the destination MAC is not known, use the spanning tree to forward the packet
+            out_ports = [port for port in self.spanning_tree if port[0] == dpid or port[1] == dpid]
+            actions = [parser.OFPActionOutput(out_port) for out_port in out_ports]
+            if not actions:
+                actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+
+        # construct action list.
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
--- p2_topo.py
+++ p2_topo.py
@@ -15,7 +15,6 @@ class CustomTopo(Topo):
         s3 = self.addSwitch('s3')
         s4 = self.addSwitch('s4')
 
-
         # Create 4 hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
@@ -34,6 +33,35 @@ class CustomTopo(Topo):
         self.addLink(s3, s4)
         self.addLink(s4, s1)
 
+def construct_spanning_tree(topo):
+    """Construct a spanning tree from the given topology using Prim's algorithm."""
+    nodes = list(topo.nodes())
+    edges = list(topo.links())
+    if not nodes:
+        return []
+
+    # Initialize the spanning tree with the first node
+    spanning_tree = []
+    connected_nodes = {nodes[0]}
+    remaining_nodes = set(nodes[1:])
+
+    while remaining_nodes:
+        min_edge = None
+        for edge in edges:
+            if (edge[0] in connected_nodes and edge[1] in remaining_nodes) or \
+               (edge[1] in connected_nodes and edge[0] in remaining_nodes):
+                if min_edge is None or edge < min_edge:
+                    min_edge = edge
+
+        if min_edge is None:
+            raise ValueError("The graph is not connected")
+
+        spanning_tree.append(min_edge)
+        connected_nodes.update(min_edge)
+        remaining_nodes.difference_update(min_edge)
+
+    return spanning_tree
+
 def run():
     """Create the network, start it, and enter the CLI."""
     topo = CustomTopo()
@@ -41,9 +69,20 @@ def run():
     net.addController('c0', controller=RemoteController, ip="127.0.0.1", protocol='tcp', port=6633)
     net.build()
     net.start()
+
+    # Construct the spanning tree
+    spanning_tree = construct_spanning_tree(topo)
+    info('*** Spanning Tree:\n')
+    for edge in spanning_tree:
+        info(f'{edge}\n')
+
+    # Save the spanning tree to a file for the controller to read
+    with open('/tmp/spanning_tree.txt', 'w') as f:
+        for edge in spanning_tree:
+            f.write(f'{edge[0]} {edge[1]}\n')
+
     info('*** Running CLI\n')
     CLI(net)
-
 
     info('*** Stopping network\n')
     net.stop()
