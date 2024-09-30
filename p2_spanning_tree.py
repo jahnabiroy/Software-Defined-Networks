c1c00e2e024e291cff724b2614acbf2e7eb362ce
Jahnabi Roy <jahnabi.royiitd@gmail.com>
2024-09-30 16:23:54 +0530

    part 2 done

--- p2_spanning_tree.py
+++ p2_spanning_tree.py
@@ -1,28 +1,42 @@
+import logging
+from logging.handlers import RotatingFileHandler
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
+from ryu.topology import event, switches
+from ryu.topology.api import *
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import ipv6  # Add this line
+from time import sleep
 
-class SimpleSwitch13(app_manager.RyuApp):
+
+class LearningSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch13, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
-        self.spanning_tree = self.load_spanning_tree('/tmp/spanning_tree.txt')
+        super(LearningSwitch, self).__init__(*args, **kwargs)
+        self.topology_api_app = self
+        self.ports_on_switch = {}
+        self.host_to_switch = {}
+        self.spanning_tree = []
+        self.forwarding_table = {}
+        self.host_to_port = {}
 
-    def load_spanning_tree(self, filepath):
-        """Load the spanning tree from a file."""
-        spanning_tree = []
-        with open(filepath, 'r') as f:
-            for line in f:
-                node1, node2 = line.strip().split()
-                spanning_tree.append((node1, node2))
-        return spanning_tree
+        # Set up logging to a file
+        log_file = "ryu_app.log"
+        # clean_log_file(log_file)
+        handler = RotatingFileHandler(
+            log_file, maxBytes=10 * 1024 * 1024, backupCount=5
+        )
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -30,79 +44,218 @@ class SimpleSwitch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # install table-miss flow entry
+        # Install a table-miss flow entry
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
+        actions = [
+            parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)
+        ]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
+            mod = parser.OFPFlowMod(
+                datapath=datapath,
+                buffer_id=buffer_id,
+                priority=priority,
+                match=match,
+                instructions=inst,
+            )
         else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
+            mod = parser.OFPFlowMod(
+                datapath=datapath, priority=priority, match=match, instructions=inst
+            )
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
+    def packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
+        in_port = msg.match["in_port"]
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
         dst = eth.dst
         src = eth.src
-
         dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        self.host_to_switch.setdefault(dpid, set())
+        out_port = ofproto.OFPP_FLOOD
+        if dst in self.forwarding_table[dpid]:
+            out_port = self.forwarding_table[dpid][dst]
 
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
-
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
-            return
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            # If the destination MAC is not known, use the spanning tree to forward the packet
-            out_ports = [port for port in self.spanning_tree if port[0] == dpid or port[1] == dpid]
-            actions = [parser.OFPActionOutput(out_port) for out_port in out_ports]
-            if not actions:
-                actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-
-        # construct action list.
-        actions = [parser.OFPActionOutput(out_port)]
-
-        # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            else:
-                self.add_flow(datapath, 1, match, actions)
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
+            self.logger.info("Calculated output port: %s", out_port)
+            self.logger.info("DPID: %s, Destination: %s, Source: %s", dpid, dst, src)
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
+        actions = []
+        if out_port != ofproto.OFPP_FLOOD:
+            actions = [parser.OFPActionOutput(out_port)]
+        else:
+            # Flood only to ports in the spanning tree
+            for port in self.ports_on_switch.get(dpid, []):
+                # if port != in_port:
+                actions.append(parser.OFPActionOutput(port))
+
+        # Install a flow to avoid packet_in next time
+        # if out_port != ofproto.OFPP_FLOOD:
+        #     match = parser.OFPMatch(in_port=in_port, eth_type=eth.ethertype, eth_dst=dst)
+        #     if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+        #         self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+        #         return
+        #     else:
+        #         self.add_flow(datapath, 1, match, actions)
+
+        # Install reverse flow for bidirectional communication
+        # reverse_match = parser.OFPMatch(in_port=out_port, eth_type=eth.ethertype, eth_dst=src)
+        # reverse_actions = [parser.OFPActionOutput(in_port)]
+        # self.add_flow(datapath, 1, reverse_match, reverse_actions)
+
+        # Construct packet out message and send it
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data,
+        )
         datapath.send_msg(out)
+
+    @set_ev_cls(event.EventSwitchEnter)
+    def get_topology_data(self, ev=None):
+        sleep(3)
+        switch_list = get_switch(self.topology_api_app, None)
+        self.switches = [switch.dp.id for switch in switch_list]
+        host_list = get_host(self.topology_api_app, None)
+
+        # Clear previous topology data
+        self.host_to_switch.clear()
+        self.host_to_port.clear()
+
+        for host in host_list:
+            if host.mac:
+                self.logger.info(
+                    "Host with mac %s is connected to switch %s",
+                    host.mac,
+                    host.port.dpid,
+                )
+                if host.port.dpid not in self.host_to_switch:
+                    self.host_to_switch[host.port.dpid] = set()
+                self.host_to_switch[host.port.dpid].add(host.mac)
+                self.host_to_port[host.mac] = host.port.port_no
+            else:
+                self.logger.info("Host %s does not have mac", host.mac)
+
+        print(self.host_to_switch)
+        links_list = get_all_link(self.topology_api_app)
+        self.links = {}
+        self.switch_ids = set()
+        for l in links_list:
+            self.switch_ids.add(l.src.dpid)
+            self.switch_ids.add(l.dst.dpid)
+            if l.src.dpid not in self.links:
+                self.links[l.src.dpid] = {}
+            if l.dst.dpid not in self.links:
+                self.links[l.dst.dpid] = {}
+            self.links[l.src.dpid][l.dst.dpid] = l.src.port_no
+            self.links[l.dst.dpid][l.src.dpid] = l.dst.port_no
+
+        if self.switch_ids:  # Only create spanning tree if we have switches
+            self.create_spanning_tree()
+        else:
+            print("No switches discovered yet. Spanning tree creation deferred.")
+
+    def create_spanning_tree(self):
+        if not self.switch_ids:
+            self.logger.warning("No switches to create spanning tree.")
+            return
+
+        # Step 1: Find edges for the spanning tree
+        spt_edges = self.find_spt_edges()
+
+        # Step 2: Use the edges to construct the spanning tree and forwarding table
+        self.construct_spt_and_forwarding_table(spt_edges)
+
+    def find_spt_edges(self):
+        edges = []
+        connected_switches = set()
+
+        # Start with an arbitrary switch
+        start_switch = next(iter(self.switch_ids))
+        connected_switches.add(start_switch)
+
+        while len(connected_switches) < len(self.switch_ids):
+            new_edges = []
+            for switch in list(
+                connected_switches
+            ):  # Create a list to avoid modifying during iteration
+                for neighbor, port in self.links.get(switch, {}).items():
+                    if neighbor not in connected_switches:
+                        new_edges.append((switch, neighbor))
+
+            if not new_edges:
+                break  # No more edges to add, graph might be disconnected
+
+            # Add the first new edge found
+            edges.append(new_edges[0])
+            connected_switches.add(new_edges[0][1])
+            connected_switches.add(new_edges[0][0])
+
+        # print("Spanning Tree Edges:", edges)
+        spt_edges = {}
+        for edge in edges:
+            if edge[0] not in spt_edges:
+                spt_edges[edge[0]] = {}
+            if edge[1] not in spt_edges:
+                spt_edges[edge[1]] = {}
+
+            spt_edges[edge[0]][edge[1]] = self.links[edge[0]][edge[1]]
+            spt_edges[edge[1]][edge[0]] = self.links[edge[1]][edge[0]]
+
+        return spt_edges
+
+    def construct_spt_and_forwarding_table(self, spt_edges):
+        self.spanning_tree = spt_edges
+        self.forwarding_table = {switch: {} for switch in self.switch_ids}
+
+        def bfs(node, parent, original_switch, original_port, visited):
+            if visited[node]:
+                return
+            visited[node] = True
+            for neighbor in spt_edges[node]:
+                if neighbor != parent:
+                    self.forwarding_table[original_switch][neighbor] = original_port
+                    bfs(neighbor, node, original_switch, original_port, visited)
+
+        for switch in self.switch_ids:
+            visited = {sw: False for sw in self.switch_ids}
+            for neighbor in spt_edges[switch]:
+                port = self.links[switch][neighbor]
+                self.forwarding_table[switch][neighbor] = port
+                bfs(neighbor, switch, switch, port, visited)
+
+        for switch in self.switch_ids:
+            self.host_to_switch.setdefault(switch, set())
+            for host_mac in self.host_to_switch[switch]:
+                self.forwarding_table[switch][host_mac] = self.host_to_port[host_mac]
+                for switch2 in self.switch_ids:
+                    if switch2 != switch:
+                        self.forwarding_table[switch2][host_mac] = (
+                            self.forwarding_table[switch2][switch]
+                        )
+
+        for switch in self.switch_ids:
+            self.ports_on_switch[switch] = list(
+                set(self.forwarding_table[switch].values())
+            )
+
+        self.logger.info("Ports on each switch: %s", self.ports_on_switch)
+        self.logger.info("Forwarding Table: %s", self.forwarding_table)
+        self.logger.info("Host to Switch: %s", self.host_to_switch)
+        self.logger.info("Host to Port: %s", self.host_to_port)
--- p2_topo.py
+++ p2_topo.py
@@ -6,20 +6,21 @@ from mininet.node import OVSSwitch, RemoteController
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
+
 class CustomTopo(Topo):
     """Custom topology with 4 switches forming a cycle and 1 host per switch."""
 
     def build(self):
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
-        s4 = self.addSwitch('s4')
+        s1 = self.addSwitch("s1")
+        s2 = self.addSwitch("s2")
+        s3 = self.addSwitch("s3")
+        s4 = self.addSwitch("s4")
 
         # Create 4 hosts
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
+        h1 = self.addHost("h1")
+        h2 = self.addHost("h2")
+        h3 = self.addHost("h3")
+        h4 = self.addHost("h4")
 
         # Connect each host to its respective switch
         self.addLink(h1, s1)
@@ -33,61 +34,24 @@ class CustomTopo(Topo):
         self.addLink(s3, s4)
         self.addLink(s4, s1)
 
-def construct_spanning_tree(topo):
-    """Construct a spanning tree from the given topology using Prim's algorithm."""
-    nodes = list(topo.nodes())
-    edges = list(topo.links())
-    if not nodes:
-        return []
-
-    # Initialize the spanning tree with the first node
-    spanning_tree = []
-    connected_nodes = {nodes[0]}
-    remaining_nodes = set(nodes[1:])
-
-    while remaining_nodes:
-        min_edge = None
-        for edge in edges:
-            if (edge[0] in connected_nodes and edge[1] in remaining_nodes) or \
-               (edge[1] in connected_nodes and edge[0] in remaining_nodes):
-                if min_edge is None or edge < min_edge:
-                    min_edge = edge
-
-        if min_edge is None:
-            raise ValueError("The graph is not connected")
-
-        spanning_tree.append(min_edge)
-        connected_nodes.update(min_edge)
-        remaining_nodes.difference_update(min_edge)
-
-    return spanning_tree
 
 def run():
     """Create the network, start it, and enter the CLI."""
     topo = CustomTopo()
     net = Mininet(topo=topo, switch=OVSSwitch, build=False)
-    net.addController('c0', controller=RemoteController, ip="127.0.0.1", protocol='tcp', port=6633)
+    net.addController(
+        "c0", controller=RemoteController, ip="127.0.0.1", protocol="tcp", port=6633
+    )
     net.build()
     net.start()
-
-    # Construct the spanning tree
-    spanning_tree = construct_spanning_tree(topo)
-    info('*** Spanning Tree:\n')
-    for edge in spanning_tree:
-        info(f'{edge}\n')
-
-    # Save the spanning tree to a file for the controller to read
-    with open('/tmp/spanning_tree.txt', 'w') as f:
-        for edge in spanning_tree:
-            f.write(f'{edge[0]} {edge[1]}\n')
-
-    info('*** Running CLI\n')
+    info("*** Running CLI\n")
     CLI(net)
 
-    info('*** Stopping network\n')
+    info("*** Stopping network\n")
     net.stop()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Set log level to display Mininet output
-    setLogLevel('info')
+    setLogLevel("info")
     run()
