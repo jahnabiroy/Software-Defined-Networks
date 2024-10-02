32bd95ecf654012194d3a5cb9c4a38ca00963f55
Abhinav Rajesh Shripad <122697564+33arsenic75@users.noreply.github.com>
2024-10-02 21:07:10 +0530

    Update p3_shortest_path.py

--- p3_shortest_path.py
+++ p3_shortest_path.py
@@ -1,112 +1,258 @@
+import logging
+from logging.handlers import RotatingFileHandler
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
-from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
+from ryu.topology import event, switches
+from ryu.topology.api import *
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import ipv6  # Add this line
+from time import sleep
 from ryu.lib.packet import ether_types
-import networkx as nx
 
-class ShortestPathSwitch13(app_manager.RyuApp):
+class LearningSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(ShortestPathSwitch13, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
-        self.net = nx.DiGraph()
+        super(LearningSwitch, self).__init__(*args, **kwargs)
+        self.topology_api_app = self
+        self.ports_on_switch = {}
+        self.host_to_switch = {}
+        self.spanning_tree = []
+        self.forwarding_table = {}
+        self.host_to_port = {}
+        self.blocked_ports = {}
+        self.dpid_switch = {}
+        self.link_delay = {}
+        # Set up logging to a file
+        log_file = "p3.log"
+        # clean_log_file(log_file)
+        with open(log_file, "w"):
+            pass
+
+        self.logger.propagate = False
+        if self.logger.hasHandlers():
+            self.logger.handlers.clear()
+
+        handler = RotatingFileHandler(
+            log_file, maxBytes=10 * 1024 * 1024, backupCount=5
+        )
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
+        if datapath.id not in self.dpid_switch:
+            self.dpid_switch[datapath.id] = datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
-        # install table-miss flow entry
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
+        actions = [
+            parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)
+        ]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id, priority=priority, match=match, instructions=inst)
         else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
+
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
+    def packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
+        in_port = msg.match["in_port"]
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
         dst = eth.dst
         src = eth.src
-
         dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
-
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
+            # self.logger.info("Received LLDP packet. Source: %s, Destination: %s, In Port: %s, Switch ID: %s", src, dst, in_port, dpid)
             return
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            # Compute shortest path using Dijkstra's algorithm
-            path = nx.shortest_path(self.net, src, dst, weight='weight')
-            next_hop = path[path.index(dpid) + 1]
-            out_port = self.net[dpid][next_hop]['port']
 
-        # construct action list.
-        actions = [parser.OFPActionOutput(out_port)]
+        out_port = ofproto.OFPP_FLOOD
+        self.forwarding_table.setdefault(dpid, {})
+        if dst in self.forwarding_table[dpid]:
+            out_port = self.forwarding_table[dpid][dst]
 
-        # install a flow to avoid packet_in next time
+        actions = []
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            actions = [parser.OFPActionOutput(out_port)]
+        else:
+            ports = [port.port_no for port in datapath.ports.values()]
+            for port in ports:
+                if port != in_port and port not in self.blocked_ports.get(dpid, []):
+                    actions.append(parser.OFPActionOutput(port))
+
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_src = src ,eth_dst=dst)
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
                 self.add_flow(datapath, 1, match, actions, msg.buffer_id)
                 return
             else:
                 self.add_flow(datapath, 1, match, actions)
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
+            return
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data,
+        )
         datapath.send_msg(out)
 
-    @set_ev_cls(ofp_event.EventOFPPortDescStatsReply, MAIN_DISPATCHER)
-    def port_desc_stats_reply_handler(self, ev):
-        for p in ev.msg.body:
-            self.net.add_edge(ev.msg.datapath.id, p.port_no, weight=p.curr_speed)
+    @set_ev_cls(event.EventSwitchEnter)
+    def get_topology_data(self, ev=None):
+        sleep(0.1)
+        switch_list = get_switch(self.topology_api_app, None)
+        self.switches = [switch.dp.id for switch in switch_list]
+        
+        links_list = get_all_link(self.topology_api_app)
+        self.links = {}
+        self.switch_ids = set()
+        for l in links_list:
+            self.switch_ids.add(l.src.dpid)
+            self.switch_ids.add(l.dst.dpid)
+            if l.src.dpid not in self.links:
+                self.links[l.src.dpid] = {}
+            if l.dst.dpid not in self.links:
+                self.links[l.dst.dpid] = {}
+            self.links[l.src.dpid][l.dst.dpid] = l.src.port_no
+            self.links[l.dst.dpid][l.src.dpid] = l.dst.port_no
+            src_mac = l.src.hw_addr  # Get source port MAC address
+            dst_mac = l.dst.hw_addr  # Get destination port MAC address
+            # self.logger.info("Link between %s and %s on port %s", src_mac, dst_mac, l.src.port_no)
+            self.send_packet(l.src.dpid,l.dst.dpid,l.src.port_no,dst_mac,None)
 
-    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, CONFIG_DISPATCHER])
-    def state_change_handler(self, ev):
-        datapath = ev.datapath
-        if ev.state == MAIN_DISPATCHER:
-            self.net.add_node(datapath.id)
-        elif ev.state == CONFIG_DISPATCHER:
-            self.net.remove_node(datapath.id)
+
+        if self.switch_ids:
+            self.create_spanning_tree()
+        else:
+            print("No switches discovered yet. Spanning tree creation deferred.")
+        
+        # self.get_topology_data(self.topology_api_app)
+
+    def create_spanning_tree(self):
+        if not self.switch_ids:
+            self.logger.warning("No switches to create spanning tree.")
+            return
+
+        spt_edges = self.find_spt_edges()
+        self.construct_spt_and_forwarding_table(spt_edges)
+
+    def find_spt_edges(self):
+        edges = []
+        connected_switches = set()
+
+        start_switch = next(iter(self.switch_ids))
+        connected_switches.add(start_switch)
+
+        while len(connected_switches) < len(self.switch_ids):
+            new_edges = []
+            for switch in list(connected_switches):  
+                for neighbor, port in self.links.get(switch, {}).items():
+                    if neighbor not in connected_switches:
+                        new_edges.append((switch, neighbor))
+            if not new_edges:
+                break
+
+            edges.append(new_edges[0])
+            connected_switches.add(new_edges[0][1])
+            connected_switches.add(new_edges[0][0])
+
+        spt_edges = {}
+        for edge in edges:
+            if edge[0] not in spt_edges:
+                spt_edges[edge[0]] = {}
+            if edge[1] not in spt_edges:
+                spt_edges[edge[1]] = {}
+
+            spt_edges[edge[0]][edge[1]] = self.links[edge[0]][edge[1]]
+            spt_edges[edge[1]][edge[0]] = self.links[edge[1]][edge[0]]
+
+        return spt_edges
+
+    def construct_spt_and_forwarding_table(self, spt_edges):
+        self.spanning_tree = spt_edges
+        self.forwarding_table = {switch: {} for switch in self.switch_ids}
+
+        def bfs(node, parent, original_switch, original_port, visited):
+            if visited[node]:
+                return
+            visited[node] = True
+            for neighbor in spt_edges[node]:
+                if neighbor != parent:
+                    self.forwarding_table[original_switch][neighbor] = original_port
+                    bfs(neighbor, node, original_switch, original_port, visited)
+
+        for switch in self.switch_ids:
+            visited = {sw: False for sw in self.switch_ids}
+            for neighbor in spt_edges[switch]:
+                port = self.links[switch][neighbor]
+                self.forwarding_table[switch][neighbor] = port
+                bfs(neighbor, switch, switch, port, visited)
+
+        for switch in self.switch_ids:
+            self.ports_on_switch[switch] = list(
+                set(self.forwarding_table[switch].values())
+            )
+        
+        for switch1 in self.switch_ids:
+            for switch2 in self.links[switch1]:
+                if self.links[switch1][switch2] != self.forwarding_table[switch1][switch2]:
+                    self.blocked_ports.setdefault(switch1, set()).add(self.links[switch1][switch2])
+                    self.blocked_ports.setdefault(switch2, set()).add(self.links[switch2][switch1])
+
+        self.logger.info("Ports on each switch: %s", self.ports_on_switch)
+        self.logger.info("Forwarding Table: %s", self.forwarding_table)
+        self.logger.info("Blocked Ports: %s", self.blocked_ports)
+
+    def send_packet(self, src_dpid, dst_dpid, port, mac_address, data):
+        import time
+        datapath = self.dpid_switch.get(src_dpid,None)
+        if not datapath:
+            self.logger.error("Datapath not found for dpid: %s", src_dpid)
+            return
+
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Measure latency
+        start_time = time.time()
+
+        actions = [parser.OFPActionOutput(port)]
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=ofproto.OFP_NO_BUFFER,
+            in_port=port,
+            actions=actions,
+            data=data,
+        )
+        datapath.send_msg(out)
+
+        latency = time.time() - start_time
+        self.link_delay.setdefault(src_dpid,{})
+        self.link_delay[src_dpid][dst_dpid] = latency
+        # self.logger.info("Packet sent to %s from switch %s on port %s. Latency: %.6f seconds", mac_address, src_dpid, port, latency)
